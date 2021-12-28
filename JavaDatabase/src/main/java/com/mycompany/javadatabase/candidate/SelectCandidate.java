6234b90465232c831f0c73728150bad642d1a414
PrinceOfDarkness-123 <94347356+PrinceOfDarkness-123@users.noreply.github.com>
2021-12-28 12:46:53 +0500

    Adding statements under main method

--- JavaDatabase/src/main/java/com/mycompany/javadatabase/candidate/SelectCandidate.java
+++ JavaDatabase/src/main/java/com/mycompany/javadatabase/candidate/SelectCandidate.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.javadatabase.candidate;
 
-/**
- *
- * @author imran-kalsym
- */
+
 public class SelectCandidate {
-    
+    static final String DB_URL = "jdbc:mysql://localhost:3306/javabackenddatabase";
+   static final String USER = "root";
+   static final String PASS = "amm123!#=/+";
+    public static void main(String[] args) {
+         try {
+             Class.forName("com.mysql.cj.jdbc.Driver");
+   Connection connect = DriverManager.getConnection(DB_URL, USER, PASS);
+   Statement mystatement = connect.createStatement();
+   ResultSet output=mystatement.executeQuery("select * from candidate");
+   while(output.next())
+      {
+          System.out.println(output.getString("id")+"  "+output.getString("job_id")+" "+output.getString("candidate_name")+" "+output.getString("candidate_age") + " "+output.getString("candidate_phone_no"));  
+      }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
